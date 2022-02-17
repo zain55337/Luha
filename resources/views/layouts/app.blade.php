c67d506fd32c3df8a488c60924a323a5e2723b9c
Zain Ali <zain55337@gmail.com>
2022-02-17 13:49:28 +0100

    Theme Configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+@section('header')
+@include('layouts.partials.header')
+@show
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<body class="g-sidenav-show  bg-gray-200">
+    @include('layouts.partials.sideBar')
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        @include('layouts.partials.mainHeader')
+        @yield('mainContent')
+        @include('layouts.partials.footerWrapper')
+    </main>
+    @include('layouts.partials.footerJS')
+    @yield('customScript')
+</body>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
 </html>
---
+++ resources/views/layouts/partials/footerWrapper.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer py-4  ">
+  <div class="container-fluid">
+    <div class="row align-items-center justify-content-lg-between">
+      <div class="col-lg-6 mb-lg-0 mb-4">
+        <div class="copyright text-center text-sm text-muted text-lg-start">
+          Copyright &copy; <?php echo date('Y'); ?> | <a target="_blank" href="" style="color:#ee802e !important">Luha</a> | All rights reserved.
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
